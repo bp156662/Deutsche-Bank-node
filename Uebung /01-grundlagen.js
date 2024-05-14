724373eaaffce940a90695ade8e4abeaadd05c22
Escobar <156646749+bp156662@users.noreply.github.com>
2024-05-14 08:56:41 +0000

    mehr Aufgaben

--- Uebung /01-grundlagen.js
+++ Uebung /01-grundlagen.js
@@ -46,3 +46,55 @@ console.log('c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))');
 
 console.log('Die Länge der Hypotenuse c beträgt: ' + c);
 
+console.log('Aufgabe 6');
+console.log('Ein Kunde legt 100 Euro auf dem Sparbuch an. Jedes Jahr bekommt er 10% Zinsen. Wie viel bekommt der Kunde');
+console.log('nach zwei Jahren ausgezahlt. Beachte den Zinseszinseffekt.');
+ 
+let laufzeit = 2;
+let startkapital = 100;
+let zinssatz = 0.1;   /* Das Komma ist zur Entwicklungszeit ein Punkt. */
+ 
+let kapitalNachEinemJahr = startkapital * (1 + zinssatz);
+ 
+console.log("Kapital nach einem Jahr: " + kapitalNachEinemJahr + " EUR.");
+ 
+let kapitalNachZweiJahren = kapitalNachEinemJahr * (1 + zinssatz);
+ 
+console.log("Kapital nach zwei Jahren: " + kapitalNachZweiJahren + " EUR.");
+ 
+//let endkapital = Math.pow((startkapital * zinssatz) , laufzeit);
+ 
+let endkapital = startkapital * Math.pow(1 + zinssatz, laufzeit);
+ 
+console.log("Endkapital nach " + laufzeit + " Jahren: " + endkapital + " EUR.");
+ console.log("Aufgabe 7");
+ console.log("Die Werte aus der vorhehringen Aufgabe werden als Reihe dargestellt.");
+ 
+ endkapital = startkapital;
+ console.log(endkapital);
+//NAch dem 1 Jahr erhöht sich das Endkapital um den Faktor* 1,1
+ endkapital = startkapital * (1 + zinssatz) ;
+ console.log(endkapital);
+//NAch dem 1 Jahr erhöht sich das Endkapital um den Faktor* 1,1
+endkapital = startkapital * (1 + zinssatz) ;
+console.log(endkapital);
+//NAch dem 1 Jahr erhöht sich das Endkapital um den Faktor* 1,1
+endkapital = startkapital * (1 + zinssatz) ;
+console.log(endkapital);
+//NAch dem 1 Jahr erhöht sich das Endkapital um den Faktor* 1,1
+endkapital = startkapital * (1 + zinssatz) ;
+console.log(endkapital);
+ 
+ console.log("Aufgabe 8");
+ console.log("In aufgabe sieben wurde die Anweisung endkapital = ... mehrfach wiederholt");
+console.log("um sich Tiparbeit zu Sparen und die wiederholung Anweisung in der gewünschten Häufigkeit durchzuführen ");
+console.log("nutzt der Programierer eine Schleife.");
+ 
+startkapital = 100;
+endkapital = startkapital;
+zinssatz = 0.1;
+laufzeit= 3;
+ for (let i = 0; 0 < laufzeit; i++) {endkapital = endkapital * (1+ zinssatz);
+    console.log("endkapitalNachJahr"+ (i+1) + ": " +endkapital + "EUR.");}
+
+ 
