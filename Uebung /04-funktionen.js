fd41e5a8343dadbad5be9b7716d2036a05e5cf97
Escobar <156646749+bp156662@users.noreply.github.com>
2024-11-11 08:05:26 +0000

    deep

--- Uebung /04-funktionen.js
+++ Uebung /04-funktionen.js
@@ -1,59 +1,101 @@
 console.log("FUNKTIONEN")
 console.log("==========")
 
-//funktionen führen operationen aus
+// Funktionen führen Operationen aus.
 
 let laenge = 3
 let breite = 5
 
-let flaecheninhalt = laenge+breite 
-console.log("Flaecheninhalt:" + flaecheninhalt)
+let flaecheninhalt = laenge * breite
 
-//jetzt die berechnung in der funktion
-//zuerst wird sie erstellt
+console.log("Flächeninhalt: " + flaecheninhalt)
 
-function berechneflaecheninhalt (breite,länge) {
-return laenge + breite
+// Jetzt die Berechnung in einer Funktion:
+
+// Zuerst wird die Funktion erstellt:
+
+function berechneFlaecheninhalt(laenge, breite){
+    return laenge * breite
 }
 
-//schritt zwei ruft die funktion ein
+// In einem zweiten Schritt wird die Funktion aufgerufen:
 
-console.log("Flaecheninhalt:" + berechneflaecheninhalt(10,10))
+console.log("Flächeninhalt: " + berechneFlaecheninhalt(10,10))
 
-// der vorteil ist, das die berechnung nur einmal anmgeführt werden muss
+// Der Vorteil von Funktionen ist, dass die Berechnung im Rumpf nur einmal ausformuliert werden muss.
+// Die Funktion kann dann an vielen Stellen aufgerufen werden.
 
-//so werden funktionen aufgestellt
-// 1. das reservierte wort function
-// 2. ein sprechender name; der name soll beschreiben was die funktion macht
-// 3. runder klammer am ende nehmen Parameter entgegen. parameter sind das was die funktion zur berechnung braucht 
-// funktionen können mehrere oder auch keine parameter entgegen nehmen 
-// 4. in der geschweifelten klammer wird die Logik ausformiert
-// 5. mit dem reservierten wort return wird das ergebnis zurückgegeben 
+// So werden Funktionen erstellt:
+
+// 1. Das reservierte Wort function 
+// 2. Ein sprechender Name. D.h., der Name soll beschreiben, was die Funktion macht.
+// 3. Runde Klammern am Ende nehmen Parameter entgegen. Parameter sind das was die Funktion als
+//    Input zur Berechnung benötigt. Funktinen können mehrere oder auch keinen Parameter entgegennehmen
+// 4. In den geschweiften Klammern wird die Logik ausprogrammiert.
+// 5. Mit dem reservierten Wort return wird das Ergebnis zurückgegeben.
 
 console.log("Aufgabe 1")
-//berechne das Volumen eines Quaders mit einer funktion
+console.log("---------")
+// Berechne das Volumen eines Quaders mit einer Funktion:
 
-def volumen_quader(l, b, h):
-    """
-    Berechnet das Volumen eines Quaders.
-    
-    :param l: 5
-    :param b: 3
-    :param h: 4
-    :return: Volumen des Quaders
-    """
-    return l * b * h
-
-
-volumen = volumen_quader(laenge, breite, hoehe)
-print(f"Das Volumen des Quaders beträgt: {volumen} Kubikeinheiten.")
-
-console.log("Aufgabe 2")
-//berechne brutto aus dem netto
-
-function berechneBrutto (nettopreis,mehrwertsteuer){
-    return nettopreis+mehrwertsteuer * nettopreis
+function berechneVolumen(laenge,breite,hoehe){
+    return laenge * breite * hoehe
 }
 
-console.log("bruttopreis:" +)
+console.log("Volumen: " + berechneVolumen(5,6,7))
+
+console.log("Aufgabe 2")
+console.log("---------")
+// Berechne den Bruttopreis aus dem Nettopreis:
+
+function berechneBruttopreis(nettopreis, mehrwertsteuersatz){
+    return nettopreis + mehrwertsteuersatz * nettopreis
+}
+
+console.log("Bruttopreis: " + berechneBruttopreis(100,0.19))
+
+console.log("Aufgabe 3")
+console.log("---------")
+// Berechne die Schulnote aus der Abitur-Punktzahl.
+
+function punkteZuNote(punkte){
+    if(punkte === 15) return "1+"
+    else if(punkte === 14) return "1"
+    else if(punkte === 13) return "1-"
+    else if(punkte === 12) return "2+"
+    else if(punkte === 11) return "2"
+    else if(punkte === 10) return "2-"    
+    
+}
+
+let punkte = 10;
+
+let note = punkteZuNote(10)
+
+console.log("Note: " + note)
+
+console.log("Aufgabe 4")
+console.log("---------")
+
+// Funktionen können auch innerhalb von Klassen definiert werden.
+// Beispiel: Kredit
+
+class Kredit{
+    constructor(){
+        this.Laufzeit
+        this.Betrag
+        this.Zinssatz
+    }
+
+    berechneGesamtkostenAmEndeDerLaufzeit(){
+        return this.Betrag * Math.pow((1 + this.Zinssatz), this.Laufzeit)
+    }
+}
+
+let kredit = new Kredit()
+kredit.Betrag = 100
+kredit.Laufzeit = 2
+kredit.Zinssatz = 0.1
+
+console.log("Gesamtkosten: " + kredit.berechneGesamtkostenAmEndeDerLaufzeit())
 
